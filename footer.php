9b4f4d4d286af5628162ce98f70a56a9c1ab8890
ZINTHUHTET-P000\ZinThuHtet <scm.zinthuhtet@gmail.com>
2022-06-09 16:14:28 +0630

    - updated map design

--- footer.php
+++ footer.php
@@ -1,52 +1,50 @@
 </main>
 
-		<!----------------------------------------------------------------------->
-		<!--##### アクセス #####-->
-		<div class="access">
-			<div class="access__container" data-aos="fade-up" data-aos-duration="1500">
-				<h2 class="index__ttl">運営元</h2>
-				<div class="access__contents">
-					<iframe src="https://www.google.com/maps/d/u/0/embed?mid=13TgSH-rQ85svVGJy8Wn39qFblb-kSXs&ehbc=2E312F&z=13" width="400" height="400"></iframe>
-					<div class="access__item">
-						<h5 class="access__ttl">ミユキホイクエン</h5>
-						<p class="access__txt">
-							&#12306;060-0005<br>
-							北海道札幌市中央区北5条西6丁目1番23号<br>
-							北海道通信ビル3F
-						</p>
-						<p class="access__txt">
-							<strong>営業時間</strong><br>
-							10:00～22:00（最終受付／21：00）
-						</p>
-						<p class="access__txt">
-							<strong>定休日</strong><br>
-							日曜日・祝日
-						</p>
-						<p class="access__txt">
-							<strong>駐車場</strong><br>
-							あり
-						</p>
-						<a class="access__tel" href="tel:0115966541">TEL  011-596-6541</a>
-					</div>
-				</div>
-			</div>
+<!----------------------------------------------------------------------->
+<!--##### アクセス #####-->
+<div class="access">
+  <div class="access__container" data-aos="fade-up" data-aos-duration="1500">
+    <h2 class="index__ttl">運営元</h2>
+    <div class="access__contents">
+      <iframe src="https://www.google.com/maps/d/u/0/embed?mid=13TgSH-rQ85svVGJy8Wn39qFblb-kSXs&ehbc=2E312F&z=13" width="500" height="500"></iframe>
+      <div class="access__item">
+        <h5 class="access__ttl">美友希保育園</h5>
+        <p class="access__txt">
+          &#12306;001-0908 <br>
+          札幌市北区新琴似８条１丁目２－５宮崎ビル１F </p>
+        <a class="access__tel" href="tel:0117378026">TEL　011-737-8026</a>
 
-			<!--##### フッター #####-->
-			<footer>
-				<div class="container">
-					<div class="footer__sns">
-						<a class="footer__icon" href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/common/footer_icon_fb.png" alt="Facebook"></a>
-						<a class="footer__icon" href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/common/footer_icon_insta.png" alt="Instagram"></a>
-					</div>
-					<small>©2022 美友希保育園</small>
-				</div>
-			</footer><!-- END / footerフッターここまで -->
-		</div><!-- END / .access アクセスここまで -->
+				<h5 class="access__ttl">もみの木新名保育園</h5>
+        <p class="access__txt">
+          &#12306;001-0910 <br>
+          札幌市北区新琴似１０条３丁目１−２</p>
+        <a class="access__tel" href="tel:0113746956">TEL　011-374-6956 </a>
 
-	</div><!-- END / #scroll__contents -->
+				<h5 class="access__ttl">バンビ保育園</h5>
+        <p class="access__txt">
+          &#12306;001-0022 <br>
+          札幌市北区２２条西４丁目１－１アカシアビル１F</p>
+        <a class="access__tel" href="tel:0112140205">TEL　011-214-0205 </a>
+      </div>
+    </div>
+  </div>
 
-	<!--##### ページトップに戻るボタン #####-->
-	<div id="page_top"><a href="#"></a></div>
+  <!--##### フッター #####-->
+  <footer>
+    <div class="container">
+      <div class="footer__sns">
+        <a class="footer__icon" href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/common/footer_icon_fb.png" alt="Facebook"></a>
+        <a class="footer__icon" href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/common/footer_icon_insta.png" alt="Instagram"></a>
+      </div>
+      <small>©2022 美友希保育園</small>
+    </div>
+  </footer><!-- END / footerフッターここまで -->
+</div><!-- END / .access アクセスここまで -->
+
+</div><!-- END / #scroll__contents -->
+
+<!--##### ページトップに戻るボタン #####-->
+<div id="page_top"><a href="#"></a></div>
 
 </div><!-- END / #page -->
 
@@ -61,4 +59,5 @@
 
 <?php wp_footer(); ?>
 </body>
+
 </html>
